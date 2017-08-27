6a9d5d8b3d7dd81a8ab70949266eccbbbe9e4940
Justin Ely <justin.ely@deciphernow.com>
2017-08-27 12:39:37 -0400

    update lc to 0.6.2, update build scripts to current syntax

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+import argparse
 from subprocess import Popen,PIPE
 import subprocess
 import shutil
@@ -6,38 +7,60 @@ import os
 import glob
 import sys
 
-all_packages = [item for item in glob.glob('./*/') if os.path.isdir(item)]
+#-------------------------------------------------------------------------------
 
-for package in all_packages:
-    print("Building {}".format(package))
-    regex_pattern = '\# \$ anaconda upload (.+\.tar\.bz2)'
-    process = Popen(['conda', 'build', package], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout, stderr)
+def parse_args():
+    '''
+    Parse command line arguments.  Returns args object.
+    '''
+    parser = argparse.ArgumentParser()
 
-    outpkg = re.findall(regex_pattern, stdout)[0]
-    process = Popen(['conda', 'convert', outpkg, '-p', 'all', '-o', package], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout, stderr)
+    parser.add_argument('pkgs', type=str, nargs='*',
+                        help='local package names to build and deploy, defaults to all.')
 
-for package in all_packages:
-    print("Building {}".format(package))
-    process = Popen(['conda', 'build', package, '--python', '3.5'], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout, stderr)
+    args = parser.parse_args()
+    return args
 
-    outpkg = re.findall(regex_pattern, stdout)[0]
-    process = Popen(['conda', 'convert', outpkg, '-p', 'all', '-o', package], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout, stderr)
+#-------------------------------------------------------------------------------
 
-for package in all_packages:
-    for root, dirs, files in os.walk(package):
-        for filename in files:
-            if not filename.endswith('.tar.bz2'):
-                continue
+if __name__ == "__main__":
+    args = parse_args()
 
-            file_to_upload = os.path.join(root, filename)
-            process = Popen(['anaconda', 'upload', '--user', 'justincely', file_to_upload, '--force'], stdout=PIPE, stderr=PIPE)
-            stdout, stderr = process.communicate()
-            print(stdout, stderr)
+    if not len(args.pkgs):
+        all_packages = [item for item in glob.glob('./*/') if os.path.isdir(item)]
+    else:
+        all_packages = args.pkgs
+
+    for package in all_packages:
+        print("Building {}".format(package))
+        regex_pattern = b'anaconda upload (.+\.tar\.bz2)'
+        process = Popen(['conda-build', package, '--python', '2.7', '--output-folder', 'lightcurve'], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout, stderr)
+
+        outpkg = re.findall(regex_pattern, stdout)[0]
+        process = Popen(['conda', 'convert', outpkg, '-p', 'all', '-f', '-o', package], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout, stderr)
+
+    for package in all_packages:
+        print("Building {} for python".format(package))
+        process = Popen(['conda-build', package, '--python', '3.5', '--output-folder', 'lightcurve'], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout, stderr)
+
+        outpkg = re.findall(regex_pattern, stdout)[0]
+        process = Popen(['conda', 'convert', outpkg, '-p', 'all', '-f', '-o', package], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout, stderr)
+
+    for package in all_packages:
+        for root, dirs, files in os.walk(package):
+            for filename in files:
+                if not filename.endswith('.tar.bz2'):
+                    continue
+
+                file_to_upload = os.path.join(root, filename)
+                process = Popen(['anaconda', 'upload', '--user', 'justincely', file_to_upload, '--force'], stdout=PIPE, stderr=PIPE)
+                stdout, stderr = process.communicate()
+                print(stdout, stderr)
